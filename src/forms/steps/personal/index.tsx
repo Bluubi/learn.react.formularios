90ec20a0d803e3592984043b72649d74c3da2d35
BluubiDev <bluubi.dev@gmail.com>
2024-05-17 23:44:28 +0200

    feat: add register field to age field

--- src/forms/steps/personal/index.tsx
+++ src/forms/steps/personal/index.tsx
@@ -2,11 +2,14 @@ import {FormGroup} from "../../form-group/form-group.tsx";
 import style from "../../../App.module.css";
 import styleForm from '../personal/form-group-wrapper.module.css';
 import {useRef} from "react";
+import {useFormContext} from "react-hook-form";
 
 export const PersonalForm = () => {
 
     const labelNameRef = useRef<HTMLLabelElement>(null);
     const labelSurnameRef = useRef<HTMLLabelElement>(null);
+
+    const { register } = useFormContext();
 
     return (
         <div className={styleForm.formGroupWrapper}>
@@ -14,7 +17,7 @@ export const PersonalForm = () => {
             <FormGroup ref={labelSurnameRef} name={'surname'}/>
             <div className={style.inputAge}>
                 <label> Edad<small>(opcional)</small> </label>
-                <input type={"number"} className={style.inputNumber} name={'number'}/>
+                <input type={"number"} className={style.inputNumber} { ...register('age')}/>
             </div>
         </div>
         )
